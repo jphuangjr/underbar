1b7c92cd1e35289613502c21d2db7b3d2ff2e1bb
Joshua Huang <jphuangjr@gmail.com>
2015-10-24 13:15:03 -0700

    Start work on Underbar. Completed: first, last, each, filter, reject

--- src/underbar.js
+++ src/underbar.js
@@ -37,6 +37,7 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    return n === undefined ? array[array.length-1] : array.splice(array.length-1, n);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -45,6 +46,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(var i =0; i<collection.length; i++){
+        iterator(collection[i]);
+      }
+    }
+    else{
+      for(var key in collection){
+        iterator(collection[key])
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -66,16 +77,28 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var passed = [];
+    _.each(collection, function(value){
+      if(test(value)){
+        passed.push(value);
+      }
+    })
+    return passed;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(value){
+      return !test(value);
+    })
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    //TODO: duplicate-free version of array.
+
   };
 
 
